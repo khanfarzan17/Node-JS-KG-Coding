0e3f716a83221d58c3b5235c47753435cf95686f
khanfarzan17 <khanfarzan200@gmail.com>
2025-10-07 21:39:04 +0530

    EJS

--- Chapter-12-kg-coding/app.js
+++ Chapter-12-kg-coding/app.js
@@ -2,22 +2,15 @@ const express=require('express');
 
 const app=express();
 
-app.get("/",(req,res,next)=>{
-  console.log('Welcome to home page ');
-  res.send('<h1> welcome to Home page </h1>')
-  
-});
+app.set('view engine','ejs');
 
-app.get("/contact",(req,res,next)=>{
-  console.log('welcome to contact page');
-  res.send('<h2> Contact page </h2>')
- 
+app.get('/',(req,res)=>{
+res.render('home',{name:'Farzan',date:new Date().toDateString(),place:'Bengalurur'});
 })
 
-app.use("",(req,res,next)=>{
-  res.send('<h2> Error Page </h2>')
-});
-
+app.use((req,res)=>{
+  res.status(404).render('error')
+})
 
 const PORT=3000;
 
